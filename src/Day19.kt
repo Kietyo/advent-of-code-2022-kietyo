f4f5563e4d88e8802c7cf884787c09605e8debb5
Xeno <xenotactic.game@gmail.com>
2022-12-27 16:11:13 -0800

    d19 timing

--- src/Day19.kt
+++ src/Day19.kt
@@ -273,7 +273,10 @@ fun main() {
             acc * i
         }
 
+        val endTime = System.currentTimeMillis()
+
         println("product: $product")
+        println((endTime - startTime).milliseconds)
     }
 
     val dayString = "day19"
@@ -281,11 +284,11 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("${dayString}_test")
 //    part1(testInput)
-//        part2(testInput)
+        part2(testInput)
 
     val input = readInput("${dayString}_input")
 //            part1(input)
-            part2(input)
+//            part2(input)
 }
 
 
