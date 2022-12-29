252b6f4088d187f7d12412d039fad04eef8abee0
Xeno <xenotactic.game@gmail.com>
2022-12-28 20:14:25 -0800

    d21 cleanup

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,134 +1,57 @@
 import java.text.NumberFormat
 
 fun main() {
-    fun part1(input: List<String>): Unit {
-        val monkeyMathWorld = mutableMapOf<
-                String,
-                    () -> Long>()
 
-        input.forEach {
-            val line = it
+    fun part1(input: List<String>): Unit {
+        val monkeyMathWorld = mutableMapOf<String, () -> Long>()
+        input.forEach { line ->
             val (monkeyName, calcString) = line.split(": ")
             val calcSplit = calcString.split(" ")
-            println(monkeyName)
-            println(calcSplit)
             when (calcSplit.size) {
-                1 -> {
-                    monkeyMathWorld[monkeyName] = { calcSplit[0].toLong() }
-                }
-
+                1 -> monkeyMathWorld[monkeyName] = { calcSplit[0].toLong() }
                 3 -> {
-                    monkeyMathWorld[monkeyName] =
-                        when (calcSplit[1]) {
-                            "+" -> {
-                                { monkeyMathWorld[calcSplit[0]]!!() + monkeyMathWorld[calcSplit[2]]!!() }
-                            }
-
-                            "-" -> {
-                                { monkeyMathWorld[calcSplit[0]]!!() - monkeyMathWorld[calcSplit[2]]!!() }
-                            }
-
-                            "*" -> {
-                                { monkeyMathWorld[calcSplit[0]]!!() * monkeyMathWorld[calcSplit[2]]!!() }
-                            }
-
-                            "/" -> {
-                                { monkeyMathWorld[calcSplit[0]]!!() / monkeyMathWorld[calcSplit[2]]!!() }
-                            }
-
-                            else -> TODO()
-                        }
-
+                    val leftCalc = { monkeyMathWorld[calcSplit[0]]!!() }
+                    val rightCalc = { monkeyMathWorld[calcSplit[2]]!!() }
+                    monkeyMathWorld[monkeyName] = when (calcSplit[1]) {
+                        "+" -> { { leftCalc() + rightCalc() } }
+                        "-" -> { { leftCalc() - rightCalc() } }
+                        "*" -> { { leftCalc() * rightCalc() } }
+                        "/" -> { { leftCalc() / rightCalc() } }
+                        else -> TODO()
+                    }
                 }
             }
         }
-
         println("root: " + monkeyMathWorld["root"]!!())
-
-    }
-
-    fun log(
-        monkeyMathWorld: MutableMap<String, () -> Long>,
-        calcSplit: List<String>
-    ) {
-        if (calcSplit.size < 3) return
-        //        val (left, op, right) = calcSplit
-        //        if (left == "humn" || right == "humn") {
-        //            val leftCalc = monkeyMathWorld[left]!!()
-        //            val rightCalc = monkeyMathWorld[right]!!()
-        //            println(calcSplit)
-        //            println("leftCalc: $leftCalc, rightCalc: $rightCalc")
-        //        }
     }
 
     fun part2(input: List<String>): Unit {
-        val monkeyMathWorld = mutableMapOf<
-                String,
-                    () -> Long>()
-
-        val rootLeft = "bjgs"
-        val rootRight = "tjtt"
-
+        val monkeyMathWorld = mutableMapOf<String, () -> Long>()
         input.forEach {
             val line = it
             val (monkeyName, calcString) = line.split(": ")
             val calcSplit = calcString.split(" ")
-            println(monkeyName)
-            println(calcSplit)
             when (calcSplit.size) {
-                1 -> {
-                    monkeyMathWorld[monkeyName] = { calcSplit[0].toLong() }
-                }
-
+                1 -> monkeyMathWorld[monkeyName] = { calcSplit[0].toLong() }
                 3 -> {
-                    if (monkeyName == "root") {
-                        monkeyMathWorld[monkeyName] = {
-                            if (monkeyMathWorld[calcSplit[0]]!!() == monkeyMathWorld[calcSplit[2]]!!()) 1L else 0L
-                        }
-                    } else {
-                        monkeyMathWorld[monkeyName] =
-                            when (calcSplit[1]) {
-                                "+" -> {
-                                    {
-                                        log(monkeyMathWorld, calcSplit)
-                                        monkeyMathWorld[calcSplit[0]]!!() + monkeyMathWorld[calcSplit[2]]!!()
-                                    }
-                                }
-
-                                "-" -> {
-                                    {
-                                        log(monkeyMathWorld, calcSplit)
-                                        monkeyMathWorld[calcSplit[0]]!!() - monkeyMathWorld[calcSplit[2]]!!()
-                                    }
-                                }
-
-                                "*" -> {
-                                    {
-                                        log(monkeyMathWorld, calcSplit)
-                                        monkeyMathWorld[calcSplit[0]]!!() * monkeyMathWorld[calcSplit[2]]!!()
-                                    }
-                                }
-
-                                "/" -> {
-                                    {
-                                        log(monkeyMathWorld, calcSplit)
-                                        monkeyMathWorld[calcSplit[0]]!!() / monkeyMathWorld[calcSplit[2]]!!()
-                                    }
-                                }
-
-                                else -> TODO()
-                            }
+                    val leftCalc = { monkeyMathWorld[calcSplit[0]]!!() }
+                    val rightCalc = { monkeyMathWorld[calcSplit[2]]!!() }
+                    monkeyMathWorld[monkeyName] = when (calcSplit[1]) {
+                        "+" -> { { leftCalc() + rightCalc() } }
+                        "-" -> { { leftCalc() - rightCalc() } }
+                        "*" -> { { leftCalc() * rightCalc() } }
+                        "/" -> { { leftCalc() / rightCalc() } }
+                        else -> TODO()
                     }
                 }
             }
         }
 
-                monkeyMathWorld["humn"] = { 3330805295850 }
-
+        val rootLeft = "bjgs"
+        val rootRight = "tjtt"
         var delta = 1000000000
         var currComp = monkeyMathWorld[rootLeft]!!().compareTo(monkeyMathWorld[rootRight]!!())
-
-        repeat(1) {
+        while (true) {
             val leftRootCalc = monkeyMathWorld[rootLeft]!!()
             val rightRootCalc = monkeyMathWorld[rootRight]!!()
             val compareTo = leftRootCalc.compareTo(rightRootCalc)
@@ -140,7 +63,7 @@ fun main() {
                     NumberFormat.getInstance().format(rightRootCalc)
                 }, humnCalc: $humnCalc, compareTo: $compareTo, delta: $delta"
             )
-//            if (leftRootCalc == rightRootCalc) break
+            if (leftRootCalc == rightRootCalc) break
             if (currComp != compareTo) {
                 currComp = compareTo
                 delta /= 10
@@ -148,21 +71,18 @@ fun main() {
             monkeyMathWorld["humn"] = { humnCalc + (delta * compareTo) }
         }
 
-        //        println("root: " + monkeyMathWorld["root"]!!())
     }
 
     val dayString = "day21"
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("${dayString}_test")
-    //    part1(testInput)
+    val testInput = readInput("${dayString}_test") //        part1(testInput)
     //    part2(testInput)
 
     val input = readInput("${dayString}_input")
-    //    part1(input)
-    // 3330805295851 too high
+//    part1(input) // 3330805295851 too high
     // Actually answer is 3330805295850, likely due to integer division rounding.
-    part2(input)
+        part2(input)
 }
 
 
